9be02e81426167985d54c40e0575eb262dba1f38
Saša Barišić <cartman300@net.hr>
2015-07-03 08:04:56 +0200

    Project files

---
+++ Tetraquark/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Tetraquark {
+	static class Program {
+		public static bool Running;
+
+		[STAThread]
+		static void Main() {
+			Console.Title = "Tetraquark Console";
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			Running = true;
+
+			TqWind Wind = new TqWind();
+			RenderWindow RWind = new RenderWindow(Wind.Handle);
+			Wind.Show();
+
+			while (Running) {
+				Application.DoEvents();
+				RWind.Clear(Color.Black);
+
+				RWind.Display();
+			}
+
+			Environment.Exit(0);
+		}
+	}
+}
---
+++ Tetraquark/TqWind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetraquark {
+	public partial class TqWind : Form {
+
+		public TqWind() {
+			InitializeComponent();
+		}
+
+		private void TqWind_Load(object sender, EventArgs e) {
+			FormClosed += (S, E) => Program.Running = false;
+		}
+	}
+}
